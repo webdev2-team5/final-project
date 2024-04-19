11083a27e0588e6161924c464c09a55494f767cd
gar7mn <gar7mn@missouri.edu>
2024-04-18 21:38:44 -0500

    instantiate posts

--- CookBookie/src/app/post.service.ts
+++ CookBookie/src/app/post.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post';
-
+import { HttpClient,HttpClientJsonpModule } from '@angular/common/http';
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  private posts:Post[] = []
+  //privately instantiate posts interface.
+  private posts:Post[] = [];
+  //create new subject
+  private postUpdate = new Subject<Post[]>()
 
-  constructor() { }
+  constructor(http:HttpClient) { }
 }
