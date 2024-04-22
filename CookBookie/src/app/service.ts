3aa2b61f16029360db1ad152b2b6052692d11d7c
Nina Rubanovich <ninaruba01@gmail.com>
2024-04-22 11:32:36 -0500

    renaming

---
+++ CookBookie/src/app/service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Post } from './recipie';
+import { HttpClient,HttpClientJsonpModule } from '@angular/common/http';
+import { Subject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  //privately instantiate posts interface.
+  private posts:Post[] = [];
+  //create new subject
+  private postUpdate = new Subject<Post[]>()
+
+  constructor(http:HttpClient) { }
+}
