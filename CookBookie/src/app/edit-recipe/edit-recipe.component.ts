ed8a054244b6008c0341efe6ac1200a402d0c6b1
gar7mn <gar7mn@missouri.edu>
2024-04-23 10:15:11 -0500

    edit recipe

--- CookBookie/src/app/edit-recipe/edit-recipe.component.ts
+++ CookBookie/src/app/edit-recipe/edit-recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {NgForm} from "@angular/forms"
-import { Recipe } from '../recipe';
-
+import {Recipe} from '../recipe'
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html',
@@ -9,7 +9,11 @@ import { Recipe } from '../recipe';
 })
 export class EditRecipeComponent {
   @Output() postedited = new EventEmitter<Recipe>()
+  constructor(public editService: RecipeService){}
   onEditRecipe(form:NgForm){
+   if (form.invalid) {
+      return;
+   }
 
   }
 }
