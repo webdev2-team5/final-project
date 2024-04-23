ac2104e6c8ff7dfb9fcc74dd6fbb1fa570adb14e
gar7mn <gar7mn@missouri.edu>
2024-04-23 15:14:38 -0500

    added comments

--- CookBookie/src/app/recipe.service.ts
+++ CookBookie/src/app/recipe.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import {HttpClient} from "@angular/common/http"
+import { Recipe } from './recipe';
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-
+  private recipes: Recipe[] = [];
   constructor(private http: HttpClient) { }
-
+  //fetch the selected recipe
   getRecipe(){
-
+    this.http.get<{message:string,recipe:Recipe[]}>('http://loaclhost:3000/api/recipes').subscribe((recipeData)=>{
+      
+    })
   }
+  //function to edit the recipe
   editRecipe(){
     
   }
