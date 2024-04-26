88c2e7ae9eaacb0b2ac702bcb24bc127cc150871
Nina Rubanovich <ninaruba01@gmail.com>
2024-04-26 16:59:06 -0500

    renaming and moving

--- CookBookie/src/app/recipe/create-recipe/create-recipe.component.ts
+++ CookBookie/src/app/recipe/create-recipe/create-recipe.component.ts
@@ -1,16 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {NgForm} from "@angular/forms"
-import {Recipe} from '../../recipe'
-import { RecipeService } from '../../recipe.service';
+import {Recipe} from '../recipe.model'
+import { RecipeService } from '../recipe.service';
 @Component({
-  selector: 'app-edit-recipe',
-  templateUrl: './edit-recipe.component.html',
-  styleUrl: './edit-recipe.component.css',
+  selector: 'app-create-recipe',
+  templateUrl: './create-recipe.component.html',
+  styleUrl: './create-recipe.component.css',
 })
-export class EditRecipeComponent {
+export class CreateRecipeComponent {
   @Output() postedited = new EventEmitter<Recipe>()
   constructor(public editService: RecipeService){}
-  onEditRecipe(form:NgForm){
+  onCreateRecipe(form:NgForm){
    if (form.invalid) {
       return;
    }
