6d6400de371b10aee5df5260fc43244de2112376
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2024-04-30 13:33:20 -0500

    Add edit/delete

--- CookBookie/src/app/recipe/recipe.service.ts
+++ CookBookie/src/app/recipe/recipe.service.ts
@@ -1,81 +1,92 @@
 import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Recipe } from './recipe.model';
-import {map} from 'rxjs/operators'
+import { map } from 'rxjs/operators';
+import { ObjectId } from 'mongoose';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RecipeService {
   private backendUrl = 'http://localhost:3000/api/recipes';
 
   private recipes: Recipe[] = [];
-  private recipeUpDate = new Subject<Recipe[]>()
+  private recipeUpDate = new Subject<Recipe[]>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   //function to get recipes by id
-  getRecipeById(recipeId:string){
-    this.http.post('http://localhost:3000/api/recipes/:id',{
-      id:recipeId
-    }).subscribe((recipe)=>{
-      console.log(recipe)
-      return recipe;
-    })
+  getRecipeById(id: string) {
+    this.http
+      .get('http://localhost:3000/api/recipes/:id', {})
+      .subscribe((recipe) => {
+        console.log(recipe);
+        return recipe;
+      });
   }
   //function to fetch recipes
-  getRecipe(){
-    this.http.get<{recipes:any}>('http://localhost:3000/api/recipes')
-    .pipe(map((recipeData)=>{
-      return recipeData.recipes.map(recipe => {
-        return {
-          id:recipe._id,
-          name:recipe.name,
-          ingredients:recipe.ingredients,
-          instructions:recipe.instructions,
-          favorited:recipe.favorited,
-          createdAt:recipe.createdAt
-        }
-      })
-    }))
-    .subscribe((transformedPost)=>{
-      this.recipes = transformedPost;
-      this.recipeUpDate.next([...this.recipes]);
-    })
+  getRecipe() {
+    this.http
+      .get<{ recipes: any }>('http://localhost:3000/api/recipes')
+      .pipe(
+        map((recipeData) => {
+          return recipeData.recipes.map((recipe) => {
+            return {
+              id: recipe._id,
+              name: recipe.name,
+              ingredients: recipe.ingredients,
+              instructions: recipe.instructions,
+              favorited: recipe.favorited,
+              createdAt: recipe.createdAt,
+            };
+          });
+        })
+      )
+      .subscribe((transformedPost) => {
+        this.recipes = transformedPost;
+        this.recipeUpDate.next([...this.recipes]);
+      });
   }
   //function to edit the recipe
-  editRecipe(recipeid:string,title:string,ingredients:string,recipe:string){
-    var recipechange = this.getRecipeById(recipeid)
+  editRecipe(
+    recipeid: string,
+    title: string,
+    ingredients: string,
+    recipe: string
+  ) {
+    var recipechange = this.getRecipeById(recipeid);
     //this is not a working method yet and as part of this process a get request will need to be sent as well
-    const data = {'recipe':recipechange}
-    this.http.patch('http://localhost:3000/api/recipes/:id',{
-
-    }).subscribe((resp)=>{
-      console.log(resp)
-      return resp;
-    })
-
+    const data = { recipe: recipechange };
+    this.http
+      .patch('http://localhost:3000/api/recipes/:id', {})
+      .subscribe((resp) => {
+        console.log(resp);
+        return resp;
+      });
   }
   //fucntion to create recipe
-  createRecipe(title:string,ingredients:string,instructions:string){
-
-    this.http.post('http://localhost:3000/api/recipes/:id',{
-      name:title,
-      ingredients:ingredients,
-      instructions:instructions,
-      favorited:false
-    }).subscribe((resp)=>{
-
-    })
+  createRecipe(title: string, ingredients: string, instructions: string) {
+    this.http
+      .post('http://localhost:3000/api/recipes/:id', {
+        name: title,
+        ingredients: ingredients,
+        instructions: instructions,
+        favorited: false,
+      })
+      .subscribe((resp) => {});
   }
   // function to delete recipe
-  deleteRecipe(){
-
+  deleteRecipe(id: ObjectId) {
+    this.http
+      .delete('http://localhost:3000/api/recipes/' + id)
+      .subscribe(() => {
+        const updatedRecipe = this.recipes.filter((recipe) => recipe.id != id);
+        this.recipes = updatedRecipe;
+        this.recipeUpDate.next([...this.recipes]);
+      });
   }
 
   getRecipeUpdateListener() {
     return this.recipeUpDate.asObservable();
   }
-
 }
