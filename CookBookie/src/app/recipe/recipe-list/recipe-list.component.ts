24f36de0457b5d2fb45256033aa3b475cea17e15
gar7mn <gar7mn@missouri.edu>
2024-04-30 12:11:45 -0500

    recipe list,edit and create updates

--- CookBookie/src/app/recipe/recipe-list/recipe-list.component.ts
+++ CookBookie/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { Subscription } from 'rxjs';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [];
+  private recipesub:Subscription;
+  constructor(public recipeservice: RecipeService){}
+
+  ngOnInit(){
+    this.recipesub = this.recipeservice.getPostUpdateListener().subscribe((recipes)=>{
+      this.recipes = recipes
+    }) 
+  }
 
 }
