193feec00b7e4d6a09684b675cfbf58787419d92
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2024-04-29 23:06:53 -0500

    Small formatting changes

--- CookBookie/src/app/app.module.ts
+++ CookBookie/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './footer/footer.component';
-import {MatCard} from "@angular/material/card"
-import { NgForm,FormsModule } from '@angular/forms';
-import {MatInput} from '@angular/material/input'
-import {MatFormField,MatFormFieldControl,MatFormFieldModule} from "@angular/material/form-field";
+import { MatCardModule } from '@angular/material/card';
+import { NgForm, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CreateRecipeComponent } from './recipe/create-recipe/create-recipe.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonModule} from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { RecipeService } from './recipe/recipe.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -26,26 +26,22 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     HeaderComponent,
     FooterComponent,
     CreateRecipeComponent,
- ],
+  ],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatCard,
     MatExpansionModule,
-    MatFormField,
-    MatCard,
-    MatInput,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
     FormsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [
-    RecipeService,
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [RecipeService, provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
